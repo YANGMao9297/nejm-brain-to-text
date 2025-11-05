f8992b6c97d463e6f8a607bb79a2496ef846f607
YangMao <e84116277@gs.ncku.edu.tw>
2025-11-05 09:26:01 +0800

    no message

--- setup.py
+++ setup.py
@@ -5,8 +5,10 @@ setup(
     version='0.0.0',
     packages=['nejm_b2txt_utils'],
     # # Specify any packages that our package itself requires.
-    # install_requires=[
-    #     'numpy',
-    #     'g2p_en',
-    # ]
+    install_requires=[
+        'numpy',
+        'g2p_en',
+        'python-dotenv',
+        'google-generativeai'
+    ]
 )
